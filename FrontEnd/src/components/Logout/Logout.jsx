99dee97be1f0e3f7d5d165ae6b3bd94cb976014c
phamquyet9688 <117346649+phamquyet9688@users.noreply.github.com>
2023-04-12 14:40:31 +0700

    creat homepage logout

---
+++ FrontEnd/src/components/Logout/Logout.jsx
@@ -0,0 +1,27 @@
+import icon from "../../img/icon.png"
+import logout from "../../img/logout.png"
+import "./Logout.css"
+function Logout(){
+    return(
+        <>
+            <div style={{backgroundColor: 'rgb(229,231,235)', minHeight: '100vh'}}>
+                <div className="flex bg-green-500 logo">
+                    <a href='/' className="flex justify-center ml-auto mr-auto">
+                        <img src={icon} className="w-1/5"/>
+                    </a>
+                </div>
+                <div className="flex bg-gray-200 justify-center w-full">
+                    <div className="bg-gray-200 pt-9">
+                        <div className=" bg-white">
+                            <img src={logout} className="border border-white-500"/>
+                            <p className="ml-6 text-2xl justify-center py-9">Logout Success</p>
+                            <button className="ml-6 mb-3 w-[88px] h-[36px] rounded-none bg-green-500 text-center text-white">SIGN IN</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
+export default Logout;
