85f2ba4649c06f377deddb887b7daa0882c5c062
Phạm Việt Trung <phvitrung@gmail.com>
2023-04-13 10:58:05 +0700

    wallet header

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -4,6 +4,7 @@ import Homepage from "./components/Homepage/Homepage";
 import Logout from "./components/Logout/Logout";
 import Login from "./components/Login-Register/Login";
 import Register from "./components/Login-Register/Register";
+import MyWallet from "./components/MyWallet/MyWallet";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/logout" element={<Logout/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
         <Route path="/register" element={<Register/>}></Route>
+        <Route path="/my-wallet" element={<MyWallet/>}></Route>
       </Routes>
   );
 }
---
+++ FrontEnd/src/components/MyWallet/MyWallet.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import WalletHeader from "../Header/WalletHeader";
+
+const MyWallet = () => {
+    return (
+        <>
+            <WalletHeader/>
+
+        </>
+    );
+};
+
+export default MyWallet;
