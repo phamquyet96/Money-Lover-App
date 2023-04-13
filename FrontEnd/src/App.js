2bdbdc6bc8c76895e90be820ed52637db906343d
Phạm Việt Trung <phvitrung@gmail.com>
2023-04-13 09:46:43 +0700

    add register

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import {Route, Routes} from "react-router-dom";
 import Homepage from "./components/Homepage/Homepage";
 import Logout from "./components/Logout/Logout";
-import Login from "./components/Login/Login";
+import Login from "./components/Login-Register/Login";
+import Register from "./components/Login-Register/Register";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Homepage/>}></Route>
         <Route path="/logout" element={<Logout/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
+        <Route path="/register" element={<Register/>}></Route>
       </Routes>
   );
 }
