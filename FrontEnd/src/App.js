b42d4850f3731ad9c3c220cecb273b43d77139ba
m8nh <86234265+m8nh@users.noreply.github.com>
2023-04-13 10:02:20 +0700

    create forgot password

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -2,8 +2,13 @@ import './App.css';
 import {Route, Routes} from "react-router-dom";
 import Homepage from "./components/Homepage/Homepage";
 import Logout from "./components/Logout/Logout";
+<<<<<<< HEAD
 import Login from "./components/Login-Register/Login";
 import Register from "./components/Login-Register/Register";
+=======
+import Login from "./components/Login/Login";
+import Forgot from "./components/Forgot_pass/Forgot";
+>>>>>>> fcd3f79 (forgot passworld)
 
 function App() {
   return (
@@ -11,7 +16,11 @@ function App() {
         <Route path="/" element={<Homepage/>}></Route>
         <Route path="/logout" element={<Logout/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
+<<<<<<< HEAD
         <Route path="/register" element={<Register/>}></Route>
+=======
+        <Route path="/forgot" element={<Forgot/>}></Route>
+>>>>>>> fcd3f79 (forgot passworld)
       </Routes>
   );
 }
