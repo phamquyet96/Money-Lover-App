01de0eecf9e04b2455c2cce0391ce82e26f02c2a
Phạm Việt Trung <phvitrung@gmail.com>
2023-04-14 10:08:23 +0700

    my wallet

--- FrontEnd/src/components/MyWallet/MyWallet.jsx
+++ FrontEnd/src/components/MyWallet/MyWallet.jsx
@@ -5,7 +5,7 @@ import MyWalletDetail from "./MyWalletDetail";
 const MyWallet = () => {
     return (
         <>
-            <div className='h-screen bg-gray-200 justify-center'>
+            <div className='h-screen bg-gray-300 justify-center'>
                     <WalletHeader/>
                     <MyWalletDetail/>
             </div>
