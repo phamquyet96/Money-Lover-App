2dcbf935f59805e7c137d3cee9973ed498ae4bb8
phamquyet9688 <117346649+phamquyet9688@users.noreply.github.com>
2023-04-17 09:39:02 +0700

    resolve conflig app.js

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -2,13 +2,9 @@ import './App.css';
 import {Route, Routes} from "react-router-dom";
 import Homepage from "./components/Homepage/Homepage";
 import Logout from "./components/Logout/Logout";
-<<<<<<< HEAD
 import Login from "./components/Login-Register/Login";
 import Register from "./components/Login-Register/Register";
-=======
-import Login from "./components/Login/Login";
 import Forgot from "./components/Forgot_pass/Forgot";
->>>>>>> fcd3f79 (forgot passworld)
 
 function App() {
   return (
@@ -16,11 +12,8 @@ function App() {
         <Route path="/" element={<Homepage/>}></Route>
         <Route path="/logout" element={<Logout/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
-<<<<<<< HEAD
         <Route path="/register" element={<Register/>}></Route>
-=======
         <Route path="/forgot" element={<Forgot/>}></Route>
->>>>>>> fcd3f79 (forgot passworld)
       </Routes>
   );
 }
