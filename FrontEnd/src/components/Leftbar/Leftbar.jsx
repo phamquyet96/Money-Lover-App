2763532c7e8f4707603f1fcef3e8071e4d266225
m8nh <86234265+m8nh@users.noreply.github.com>
2023-04-17 10:33:29 +0700

    modal done

--- FrontEnd/src/components/Leftbar/Leftbar.jsx
+++ FrontEnd/src/components/Leftbar/Leftbar.jsx
@@ -1,13 +1,13 @@
 import {
-    Sidebar,
-    Menu,
-    MenuItem,
-    useProSidebar,
-    SubMenu,
+  Sidebar,
+  Menu,
+  MenuItem,
+  useProSidebar,
+  SubMenu,
 } from "react-pro-sidebar";
 import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
 
-import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import WidgetsOutlinedIcon from "@mui/icons-material/WidgetsOutlined";
 import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
@@ -24,108 +24,114 @@ import Tab from "@mui/material/Tab";
 import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
+
+import BasicModal from "../Modal/Modal";
+
+
+
 function Leftbar() {
-    const { collapseSidebar, toggleSidebar, toggled } = useProSidebar();
+  const { collapseSidebar, toggleSidebar, toggled } = useProSidebar();
 
-    const toggle = () => {
-        toggleSidebar();
-        if (toggled) {
-            console.log(true);
-            collapseSidebar();
-        } else {
-            console.log(false);
-            collapseSidebar();
-        }
-    };
-    const [value, setValue] = React.useState("1");
+  const toggle = () => {
+    toggleSidebar();
+    if (toggled) {
+      console.log(true);
+      collapseSidebar();
+    } else {
+      console.log(false);
+      collapseSidebar();
+    }
+  };
+  const [value, setValue] = React.useState("1");
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  
 
-    return (
+  return (
+    <div
+      id="app"
+      style={({ height: "100vh" }, { display: "flex", flexDirection: "row" })}
+    >
+      <Sidebar
+        breakPoint="sm"
+        transitionDuration={800}
+        backgroundColor="#ffff"
+        rtl={false}
+        style={{ height: "100vh" }}
+      >
+        <Menu>
+          <MenuItem
+            icon={<SavingsOutlinedIcon />}
+            onClick={() => {
+              toggle();
+            }}
+          >
+            Menu
+          </MenuItem>
+
+          <SubMenu icon={<MenuOutlinedIcon />} label="Profile">
+            <MenuItem>
+              <BasicModal/> 
+            </MenuItem>
+
+            <MenuItem icon={<AccountBalanceWalletOutlinedIcon />}>
+              Wallet
+            </MenuItem>
+            <MenuItem icon={<WidgetsOutlinedIcon />}>Category</MenuItem>
+          </SubMenu>
+
+          <MenuItem icon={<AccountBalanceWalletOutlinedIcon />}>
+            Transactions
+          </MenuItem>
+          <MenuItem icon={<AssessmentOutlinedIcon />}>Report</MenuItem>
+          <MenuItem icon={<AdUnitsOutlinedIcon />}>Budget</MenuItem>
+          <MenuItem icon={<ShoppingCartOutlinedIcon />}>Store</MenuItem>
+          <MenuItem icon={<HelpOutlineOutlinedIcon />}>FAQ</MenuItem>
+        </Menu>
+      </Sidebar>
+
+      <Container maxWidth="sm">
         <div
-            id="app"
-            style={({ height: "100vh" }, { display: "flex", flexDirection: "row" })}
+          style={{
+            backgroundColor: "white",
+            height: "30em",
+            display: "flex",
+            justifyContent: "center",
+          }}
         >
-            <Sidebar
-                breakPoint="sm"
-                transitionDuration={800}
-                backgroundColor="#ffff"
-                rtl={false}
-                style={{ height: "100vh" }}
-            >
-                <Menu>
-                    <MenuItem
-                        icon={<SavingsOutlinedIcon />}
-                        onClick={() => {
-                            toggle();
-                        }}
-                    >
-                        Menu
-                    </MenuItem>
-
-                    <SubMenu icon={<MenuOutlinedIcon />} label="Profile">
-                        <MenuItem icon={<PeopleOutlinedIcon />}>
-                            Manage my account{" "}
-                        </MenuItem>
-                        <MenuItem icon={<AccountBalanceWalletOutlinedIcon />}>
-                            Wallet
-                        </MenuItem>
-                        <MenuItem icon={<WidgetsOutlinedIcon />}>Category</MenuItem>
-                    </SubMenu>
-
-                    <MenuItem icon={<AccountBalanceWalletOutlinedIcon />}>
-                        Transactions
-                    </MenuItem>
-                    <MenuItem icon={<AssessmentOutlinedIcon />}>Report</MenuItem>
-                    <MenuItem icon={<AdUnitsOutlinedIcon />}>Budget</MenuItem>
-                    <MenuItem icon={<ShoppingCartOutlinedIcon />}>Store</MenuItem>
-                    <MenuItem icon={<HelpOutlineOutlinedIcon />}>FAQ</MenuItem>
-                </Menu>
-            </Sidebar>
-
-            <Container maxWidth="sm">
-                <div
-                    style={{
-                        backgroundColor: "white",
-                        height: "30em",
-                        display: "flex",
-                        justifyContent: "center",
-                    }}
+          <Box sx={{ width: "100%", typography: "body1" }}>
+            <TabContext value={value}>
+              <Box
+                className="flex justify-center"
+                sx={{
+                  borderBottom: 1,
+                  borderColor: "divider",
+                  color: "success.main",
+                }}
+              >
+                <TabList
+                  onChange={handleChange}
+                  aria-label="lab API tabs example"
+                  value={value}
+                  textColor="success"
+                  // indicatorColor="success"
                 >
-                    <Box sx={{ width: "100%", typography: "body1" }}>
-                        <TabContext value={value}>
-                            <Box
-                                className="flex justify-center"
-                                sx={{
-                                    borderBottom: 1,
-                                    borderColor: "divider",
-                                    color: "success.main",
-                                }}
-                            >
-                                <TabList
-                                    onChange={handleChange}
-                                    aria-label="lab API tabs example"
-                                    value={value}
-                                    textColor="success"
-                                    // indicatorColor="success"
-                                >
-                                    <Tab label="Last month" value="1" />
-                                    <Tab label="This month" value="2" />
-                                    <Tab label="Plan for future" value="3" />
-                                </TabList>
-                            </Box>
-                            <TabPanel value="1"></TabPanel>
-                            <TabPanel value="2"></TabPanel>
-                            <TabPanel value="3"></TabPanel>
-                        </TabContext>
-                    </Box>
-                </div>
-            </Container>
+                  <Tab label="Last month" value="1" />
+                  <Tab label="This month" value="2" />
+                  <Tab label="Plan for future" value="3" />
+                </TabList>
+              </Box>
+              <TabPanel value="1"></TabPanel>
+              <TabPanel value="2"></TabPanel>
+              <TabPanel value="3"></TabPanel>
+            </TabContext>
+          </Box>
         </div>
-    );
+      </Container>
+    </div>
+  );
 }
 
 export default Leftbar;
-
